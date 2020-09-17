b140dc026d6ff788ba72d6bb55a065e8475ae691
Joe Davault <joe.davault@gmail.com>
2020-09-17 01:06:03 -0700

    ST-2101/HelloChangs

---
+++ Admin/build/precache-manifest.98d604833c4ce9562f3419e1d53776e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd6f3cbdc6c1b8f39436d4154020d2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ad69b58b5941c7aa56",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "897f677d58e7aa7090cc",
+    "url": "/static/js/2.5af55477.chunk.js"
+  },
+  {
+    "revision": "89ad69b58b5941c7aa56",
+    "url": "/static/js/main.2027251c.chunk.js"
+  },
+  {
+    "revision": "e04ba31971de5f14ffce",
+    "url": "/static/js/runtime~main.d5a4890f.js"
+  }
+]);
